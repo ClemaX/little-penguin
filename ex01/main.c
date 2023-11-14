6ef1e13042c156889ec98fc4543e8a25274a0ba5
Clément Hamada <clementhamada@protonmail.com>
2023-11-14 19:09:25 +0100

    ex01: Refactor

--- ex01/main.c
+++ ex01/main.c
@@ -1,16 +1,21 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
-int init_module(void)
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Clément Hamada");
+MODULE_DESCRIPTION("Hello world module");
+
+int __init hello_world_init(void)
 {
 	pr_info("Hello world !\n");
 
 	return 0;
 }
 
-void cleanup_module(void)
+void __exit hello_world_exit(void)
 {
 	pr_info("Cleaning up module.\n");
 }
 
-MODULE_LICENSE("GPL");
+module_init(hello_world_init);
+module_exit(hello_world_exit);
